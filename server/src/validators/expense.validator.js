e7ddc02866daf891ff30856ea8791e5aa3a83bad
Rohan7207 <rohanfigredo@gmail.com>
2026-08-05 21:08:05 +0530

    Created expense validator

---
+++ server/src/validators/expense.validator.js
@@ -0,0 +1,66 @@
+const { body, validationResult } = require("express-validator");
+
+function validateRequest(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  next();
+}
+
+exports.expenseValidator = [
+  body("amount")
+    .notEmpty()
+    .withMessage("Amount is requires")
+    .isFloat({ gt: 0 })
+    .withMessage("Amount must be grater than 0")
+    .toFloat(),
+
+  body("category")
+    .notEmpty()
+    .withMessage("Category is required")
+    .isIn([
+      "Food",
+      "Travel",
+      "Shopping",
+      "Bills",
+      "Health",
+      "Education",
+      "Entertainment",
+      "Investment",
+      "Others",
+    ])
+    .withMessage("Invalid category type"),
+
+  body("paymentMethod")
+    .notEmpty()
+    .withMessage("Payment method is required")
+    .isIn(["Cash", "UPI", "Debit Card", "Credit Card", "Net Banking", "Wallet"])
+    .withMessage("Invalid payment method"),
+
+  body("expenseDate")
+    .notEmpty()
+    .withMessage("Expense Date is required")
+    .isISO8601()
+    .withMessage("Invalid date format"),
+
+  body("description")
+    .optional()
+    .trim()
+    .isLength({ max: 200 })
+    .withMessage("Description must be 200 characters only"),
+
+  body("merchant").optional()
+    .trim()
+    .isLength({ max: 100 })
+    .withMessage("Merchant name cannot exceed 100 characters");,
+
+  body("notes").optional()
+    .trim()
+    .isLength({ max: 500 })
+    .withMessage("Notes cannot exceed 500 characters"),
+
+  validateRequest,
+];
